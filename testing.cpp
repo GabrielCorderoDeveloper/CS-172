553b8d4914ab16b5f53ce74f81a2cb920a256aff
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-10 14:19:43 -0400

    First commit

---
+++ testing.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 2, b = 5, c = 6; 
+    a++;
+    b = b * a;
+    c = (c - a) + 3;
+    cout << a << endl; cout << b << endl; cout << c << endl;
+
+    return 0;
+}
+
+
+
