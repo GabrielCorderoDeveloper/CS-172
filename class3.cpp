d11aeb640eccfb0e8c9159583e1e18f071d84e7c
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-11 06:21:41 -0400

    Update class3 and test

---
+++ class3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 5, b = 8, c = 2, d , e; 
+    // d = a * c * 2 + b / 4 + c * 8;
+    // cout << "The value of D is: " << d << endl;
+
+    // e = b * (2-1) + c * a / 3;
+    // cout << "The value of E is: " << e << endl;
+
+        int a = 5, b = 10; 
+        int results = a++ + b++ - a++;
+        cout << ++results << endl;
+
+
+    return 0;
+}
+
