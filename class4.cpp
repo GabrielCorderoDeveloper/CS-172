460dd125e87c24db0ad45b095213e375114c70ab
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-12 14:57:06 -0400

    Fixed log bug by change to base 10

---
+++ class4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int a = 9, b = 4, c = 2, results; 
+    results = pow(a,c) + (log10(100) + 8) * b;
+
+    cout << "results " << results;
+    return 0;
+}
