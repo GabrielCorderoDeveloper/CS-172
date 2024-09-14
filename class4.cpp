29a74a6f250e0607e720db7b3fcb1ffc58e05c31
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-13 20:38:46 -0400

    Update class4

--- class4.cpp
+++ class4.cpp
@@ -2,11 +2,20 @@
 #include<cmath>
 using namespace std;
 
+// int main()
+// {
+//     int a = 9, b = 4, c = 2, results; 
+//     results = pow(a,c) + (log10(100) + 8) * b;
+
+//     cout << "results " << results;
+//     return 0;
+// }
 int main()
 {
-    int a = 9, b = 4, c = 2, results; 
-    results = pow(a,c) + (log10(100) + 8) * b;
+    double angle = M_PI/2, results; 
+    results = sin(angle * 2) + (cos(angle) + 5) * 3;
 
     cout << "results " << results;
     return 0;
 }
+
