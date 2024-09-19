dec7d8015efeb51f466881088abf62a73bf116db
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-19 13:57:49 -0400

    Update class5

--- class5.cpp
+++ class5.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
+    // int a = 8, b = 3, c = 2, result;
+    // result = a - b * c + ( a / b );
+
+    result = 17 % 3;
+
+    cout << result;
     return 0;
 }
