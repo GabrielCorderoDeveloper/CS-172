3c27cd2b404b4aea1f9b9753f9899cc39cd193bd
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-24 15:14:54 -0400

    Created class 6

---
+++ class6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 6; 
+
+    if (a > 5) { 
+        cout << "A is greater than 5";
+    } else {
+        cout << "A is LESSER than 5";
+    }
+    
+    return 0;
+}
