7458856be6c08e8315563a681b9d716f34ce3613
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-25 18:09:28 -0400

    Revert changes

--- class6.cpp
+++ class6.cpp
@@ -4,9 +4,8 @@ using namespace std;
 int main()
 {
     int a = 6; 
-    int process = false;
 
-    if (a > 5 && process) { 
+    if (a > 5) { 
         cout << "A is greater than 5";
     } else {
         cout << "A is LESSER than 5";
