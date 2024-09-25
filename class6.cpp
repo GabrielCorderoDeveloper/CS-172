95fb9dd9f8985047a271ea42eeb4ada68986f4f9
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-09-25 18:09:05 -0400

    Updated function

--- class6.cpp
+++ class6.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main()
 {
     int a = 6; 
+    int process = false;
 
-    if (a > 5) { 
+    if (a > 5 && process) { 
         cout << "A is greater than 5";
     } else {
         cout << "A is LESSER than 5";
