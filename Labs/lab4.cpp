73b97e1ab19b2f917f085d9e0462f9d4197994b2
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-10-08 14:10:01 -0400

    Update lab4

--- Labs/lab4.cpp
+++ Labs/lab4.cpp
@@ -1,15 +1,83 @@
 #include<iostream>
 using namespace std;
 
+/*
 int main()
 {
-    int a = 6; 
+    //PROBLEM 1*********************************
+    int x; 
+    cin >> x;
 
-    if (a > 5) { 
-        cout << "A is greater than 5";
+    if (x%2 == 0) { 
+        cout << "The number is even";
     } else {
-        cout << "A is LESSER than 5";
+        cout << "The number is odd";
+    }
+    return 0;
+
+    //PROBLEM 2*********************************
+        int age; 
+    cin >> age;
+
+    if (age < 12) { 
+        cout << "Child";
+    } else if (age >= 13 && age <= 19) {
+        cout << "Teenager";
+    } else {
+        cout << "Adult";
+    }
+    
+    return 0;
+
+    //PROBLEM 3*********************************
+    int num1, num2, num3; 
+    cout << "Input number 1" << endl;
+    cin >> num1;
+    cout << "Input number 2" << endl;
+    cin >> num2;
+    cout << "Input number 3" << endl;
+    cin >> num3;
+
+    if (num1 > num2 && num1 > num3) { 
+        cout << "The biggest number is " << num1;
+    } else if (num2 > num1 && num2 > num3) { 
+        cout << "The biggest number is " << num2;
+    } else { 
+        cout << "The biggest number is " << num3;
+    } 
+    
+    return 0;
+
+    //PROBLEM 4*********************************
+        int age, score; 
+
+    cout << "Student age" << endl;
+    cin >> age;
+    cout << "Student score" << endl;
+    cin >> score;
+
+    if (age >= 18 && score >= 70) { 
+        cout << "Admitted";
+    } else {
+        cout << "Not amitted";
+    }
+    
+    return 0;
+
+    //PROBLEM 5*********************************
+        int temperature, isRaining; 
+
+    cout << "Temperature" << endl;
+    cin >> temperature;
+    cout << "Is it raining? , input 0 for false and 1 for true" << endl;
+    cin >> isRaining;
+
+    if (temperature > 30 && !isRaining) { 
+        cout << "Hot and sunny";
+    } else {
+        cout << "Cool or rainy";
     }
     
     return 0;
 }
+*/
