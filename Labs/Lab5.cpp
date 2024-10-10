88de78471c152fb2a1197b78f5bc9ccafa5c568b
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-10-10 15:24:30 -0400

    Update LAB5

--- Labs/Lab5.cpp
+++ Labs/Lab5.cpp
@@ -1,9 +1,95 @@
 #include<iostream>
+#include <stdlib.h> 
 using namespace std;
 
 int main()
-{
-    int z = 2; 
-    cout << z-- << " " << z;
+{    
+    /* 
+    QUESTION 10-------------------
+srand((unsigned)time(0)); 
+    int i, guess = 0;
+    i = (rand()%100)+1;
+    cout << "Enter a number" << endl;
+
+    while (guess != i) {
+        cin >> guess;
+
+        if (guess > i) {
+            cout << "The number is too big" << endl;
+        } else if (guess < i){
+            cout << "The number is too SMALL!" << endl;
+
+        }
+    }
+
+    cout << endl << "NICE JOB, the number is " << i << endl;
+    
     return 0;
+
+
+    QUESTION 9-------------------
+    int balance = 1000;
+    int withdraw;
+    while (balance > 0) {
+        cout << "Enter an amount to withdraw" << endl; 
+        cin >> withdraw;
+
+        if (withdraw > balance) {
+            cout << "Insuficient balance" << endl;
+        } else {
+            cout << "$" << withdraw << " Had been retired" << endl << endl;
+            balance -= withdraw;
+        }
+    }
+
+    cout << endl << "Your current balance is $0" << endl; 
+    return 0;
+    
+    QUESTION 8-------------------
+    int number = 1;
+    while (number > 0) {
+        cout << "enter a positive number" << endl; 
+        cin >> number;
+
+        if (number >= 0) {
+            cout << "POSITIVE! " << number << endl;
+        } else {
+            cout << "NEGATIVE!!!!" << endl;
+        }
+    }
+
+    return 0;
+
+
+    QUESTION 7-------------------
+    string pass = "C++Rocks", input;
+    while (input != pass) {
+        cout << "enter a password" << endl; 
+        cin >> input;
+
+        if (input != pass) {
+            cout << "WRONG PASSWORD!" << endl;
+        } else {
+            cout << "WELCOME MR. STARTK" << endl;
+        }
+    }
+
+    return 0;
+
+    QUESTION 6-------------------
+    int z;
+    while (z > 10 || z < 1) {
+        cout << "enter a number from 1 to 10" << endl; 
+        cin >> z;
+
+        if (z > 10 || z < 1) {
+            cout << "Number outside this range" << endl;
+        } else {
+            cout << "Number OKAY" << endl;
+        }
+    }
+
+    return 0;
+    */
 }
+
