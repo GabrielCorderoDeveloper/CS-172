1d821e3c3bac4b380cedae98060fb3401c3664db
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-10-24 14:33:03 -0400

    Update lab 6

---
+++ Lab6.cpp
@@ -0,0 +1,31 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Question 8
+
+    /*
+    //Question 7 ------------------------------
+    int  sum = 0;
+    for (int i = 0; i <= 20; i++)
+    {
+        if(i % 2 != 0) {
+            sum += i;
+            cout << i << endl;
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+
+    //Question 6 ------------------------------
+      for (int i = 10; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+    */
+}
