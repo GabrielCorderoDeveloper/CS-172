333040a5a0a98b460f5faf30e9923fd8a9008f50
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-10-29 14:04:13 -0400

    Update lab 6

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,11 +1,42 @@
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
 using namespace std;
+#include <cmath>
 
 int main() {
-    //Question 8
-
+    //Question 10
+    int c;
+    for(int a = 1; a <= 20 ; a++){
+        for(int b = a; b <= 20; b++){
+            for(int c = a; c <= 20; c++) {
+                if(a*a + b*b == c*c) {
+                    cout << a << ' ' << b << ' ' << c << endl;
+                }
+            }            
+        }
+    }
+    return 0;
+}
+        
     /*
+    //Question 9
+    int sum = 0, number;
+    cout << "Enter a number" << endl;
+    cin >> number;
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+    cout << sum << endl;
+
+    //Question 8
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << pow(3, i) << endl;
+    }
+
+    return 0;
+
     //Question 7 ------------------------------
     int  sum = 0;
     for (int i = 0; i <= 20; i++)
@@ -21,11 +52,11 @@ int main() {
     return 0;
 
     //Question 6 ------------------------------
-      for (int i = 10; i >= 1; i--)
+    for (int i = 10; i >= 1; i--)
     {
         cout << i << endl;
     }
 
     return 0;
     */
-}
+
