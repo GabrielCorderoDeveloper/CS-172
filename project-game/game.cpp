b333bede61c376808b06bc6d96e866845e2e8c30
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-11-12 15:29:25 -0500

    Partially designd interface

---
+++ project-game/game.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib> 
+#include <ctime> 
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+    //Genrate random numbre from 1 to 100
+    srand((unsigned)time(0)); 
+    int randomNum = (rand()%100)+1;
+
+}
+/*
+//2!fist text:
+-"I hear you are in search of treasure traveller. There is a tresure only if you are willing to travel along a dangerous path💀. There is a forest just ahead of here which lies an ancient temple. It is said to contain a treasure of ancient times and of great value🪙."
+
+-"Choose a name for your character:"
+
+//2!Random events:
+-"You have encounter a wild animal🦁, you try to defend youself but you get hurt. -10 HP"
+
+-"You find an strange plan that glows🌿. You decided to eat some of it. It heals some of you wounds. +10 HP"
+
+-"You find and obstacle🪨, you are able to get trough it but you hurt yourself slightly. -5 HP"
+
+//2!Encounters:
+-"You reach the edge of the Forbidden Forest. Shadows loom over you, and the forest seems alive. Choose your actions cautiously!"
+-"(1) Move cautiously "
+-"(2) Rush forward"
+-"(3) Climb a tree"
+-""
+-""
+
+-""
+-""
+-""
+-""
+-""
+-""
+-""
+
+
+
+
+*/
