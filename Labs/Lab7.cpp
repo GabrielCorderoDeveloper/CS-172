52cfa2834df90d084e15dc68a499943a4f12ef8f
Gabriel Cordero <gabrielcorderoolivo@gmail.com>
2024-11-19 14:34:06 -0500

    Update lab 7

--- Labs/Lab7.cpp
+++ Labs/Lab7.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
+#include <cstdlib> 
+#include <ctime>   
 using namespace std;
 
 int main() {
+    //~QUESTION     9    ---------------------------->
+    srand(static_cast<unsigned int>(time(0))); 
+    int randomNumber = rand() % 50 + 1;      
+    int guess;                              
+
+    cout << "Guess the number (between 1 and 50): " << endl;
+
+    do {
+        cin >> guess; 
+
+        if (guess < randomNumber) {
+            cout << "Too low! Try again: " << endl;
+        } else if (guess > randomNumber) {
+            cout << "Too high! Try again: " << endl;
+        } else {
+            cout << "Congratulations! You guessed the number: " << randomNumber << endl;
+        }
+    } while (guess != randomNumber); 
+
+    return 0;
+
+    /*
+    //~QUESTION     8    ---------------------------->
+    int n; 
+    int factorial = 1; 
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+        return 1;
+    }
+
+    int i = 1; 
+    do {
+        factorial *= i; 
+        i++;           
+    } while (i <= n);  
+
+    cout << "The factorial of " << n << " is: " << factorial << endl;
+    return 0;
+
+    //~QUESTION     7    ---------------------------->
+    int number; 
+
+    do {
+        cout << "Enter a positive integer: ";
+        cin >> number; 
+
+        if (number <= 0) {
+            cout << "Error: Please enter a positive integer!" << endl;
+        }
+    } while (number <= 0); 
+
+    cout << "You entered: " << number << endl; 
+    return 0;
+
+    //~QUESTION     6    ---------------------------->
     int num = 1; 
 
     do {
@@ -9,5 +69,5 @@ int main() {
         num++;               
     } while (num <= 5);       
 
-    return 0;
+    return 0;  */
 }
